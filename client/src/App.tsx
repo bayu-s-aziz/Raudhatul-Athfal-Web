11a3d8d4494da4ebbb44248ec715abebdc05421b
bayu-s-aziz <bayu.s.aziz@gmail.com>
2025-12-27 19:51:11 +0700

    scroll to top

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Switch, Route } from "wouter";
+import { useEffect } from "react";
+import { Switch, Route, useLocation } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/ThemeProvider";
@@ -8,6 +9,17 @@ import Program from "@/pages/Program";
 import Galeri from "@/pages/Galeri";
 import Kontak from "@/pages/Kontak";
 import NotFound from "@/pages/not-found";
+
+// Scroll to top ketika route berubah
+function ScrollToTop() {
+  const [location] = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+}
 
 function Router() {
   return (
@@ -26,6 +38,7 @@ function App() {
   return (
     <ThemeProvider>
       <TooltipProvider>
+        <ScrollToTop />
         <Toaster />
         <Router />
       </TooltipProvider>
